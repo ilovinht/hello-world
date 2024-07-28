475abc63aaf405a279c4289110ed3633e77db9bb
ilovinht <54866918+ilovinht@users.noreply.github.com>
2024-07-29 03:45:23 +0900

    Update mtxx.js

--- mtxx.js
+++ mtxx.js
@@ -1,14 +1,7 @@
 /******************************
 
-脚本功能：Foodie-美食相机——解锁VIP
-下载地址：https://is.gd/RChGSr
-软件版本：5.2.11
-脚本作者：彭于晏
-更新时间：2023-9-28
-问题反馈：QQ+89996462
-QQ会员群：779392027
-TG反馈群：https://t.me/plus8889
-TG频道群：https://t.me/py996
+脚本功能：test
+更新时间：2023-7-29
 使用声明：️此脚本仅供学习与交流，请勿转载与贩卖！️️️
 
 *******************************
@@ -37,7 +30,7 @@ var obj = JSON.parse($response.body);
     "have_valid_contract" :true,
     "use_vip" : true,
     "show_renew_flag" : false,
-    "show_renew_flag_abroad" : true,
+    "show_renew_flag_abroad" : false,
     "in_trial_period" : false,
     "account_type" : 2
   },
